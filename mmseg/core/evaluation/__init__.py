5ca0a1ea9b9326016c8855153064497cd317cb40
BJUT-AI&VBD <74049616+BJUT-AIVBD@users.noreply.github.com>
2023-12-01 14:38:55 +0800

    Delete mmseg directory

--- mmseg/core/evaluation/__init__.py
+++
@@ -1,7 +0,0 @@
-from .class_names import get_classes, get_palette
-from .eval_hooks import DistEvalHook, EvalHook
-from .metrics import eval_metrics, mean_dice, mean_iou, eval_metrics_unpaved
-__all__ = [
-    'EvalHook', 'DistEvalHook', 'mean_dice', 'mean_iou', 'eval_metrics',
-    'get_classes', 'get_palette', 'eval_metrics_unpaved'
-]
